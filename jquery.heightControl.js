e64429f72b7c3d09a45c17b72de1bfe23a6b36b0
mavsan <mavsan@yandex.ru>
2016-06-07 11:37:53 +1000

    Add files via upload
    v1

---
+++ jquery.heightControl.js
@@ -0,0 +1,35 @@
+/**
+ * Created by mavsan on 07.06.2016.
+ * Author: Maksim Klimenko
+ * Email: mavsan@gmail.com
+ */
+
+(function($){
+    jQuery.fn.heightControl = function(callback){
+
+        function main() {
+            var el = $(this),
+                lastHeight = el.height(),
+                newHeight = lastHeight;
+
+            callback = typeof callback == 'undefined' ? $.noop : callback;
+
+            (function run(){
+                newHeight = el.height();
+
+                if( lastHeight != newHeight ) {
+                    callback(lastHeight, newHeight);
+                    el.trigger('heightChanged', [lastHeight, newHeight]);
+                    lastHeight = newHeight;
+                }
+
+                if( el.onElementHeightChangeTimer )
+                    clearTimeout(el.onElementHeightChangeTimer);
+
+                el.onElementHeightChangeTimer = setTimeout(run, 200);
+            })();
+        }
+
+        return this.each(main);
+    };
+})(jQuery);
